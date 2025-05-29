15e22b4812961c692bab634986f68ef4f39d0e0c
corpsinhere <broosnemanic@gmail.com>
2025-05-29 03:31:38 -0700

    Moving light source

--- scenes/chromatic_distortion_test.gd
+++ scenes/chromatic_distortion_test.gd
@@ -3,6 +3,7 @@ extends Node2D
 var sprite: Sprite2D
 @export var light_source: Node2D
 var shadow_node: Shadow2D
+var light_source_sprite: Sprite2D
 
 func _ready() -> void:
 	sprite = Sprite2D.new()
@@ -13,6 +14,7 @@ func _ready() -> void:
 	shadow_node.light_source = light_source
 	shadow_node.initialize()
 	shadow_node.position = Vector2(250.0, 250.0)
+	light_source_sprite = light_source.get_node("Icon5")
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
@@ -26,3 +28,14 @@ func _input(event):
 			light_source.position += Vector2(10.0, 0.0)
 		if event.keycode == KEY_Z:
 				shadow_node.alpha = 0.5
+
+
+func _process(delta: float) -> void:
+	var t_secs: float = Time.get_ticks_msec()
+	#var t_pos: Vector2 = Vector2(250.0 + 250.0 * (sin(t_secs / 1000.0) - cos(t_secs / 1000.0)), 250.0 + 150.0 * cos(t_secs / 1000.0))
+	var t_pos: Vector2 = shadow_node.position + Vector2(250.0 * sin(t_secs / 1000.0), 50.0 * sin(0.5 * PI + t_secs / 500.0))
+	light_source.position = t_pos
+	#var t_z: float = 200.0 + 50.0 * sin(t_secs / 500.0)
+	#shadow_node.light_source_delta_z = t_z
+	#light_source_sprite.scale = 0.4 * (t_z / 500.0) * Vector2.ONE
+	
--- scripts/shadow_2d.gd
+++ scripts/shadow_2d.gd
@@ -2,21 +2,21 @@
 extends Node2D
 class_name Shadow2D
 
-@export var sprite: Sprite2D:			# The sprite that will be casting a shadow
+@export var sprite: Sprite2D:					# The sprite that will be casting a shadow
 	set(a_sprite):
 		sprite = a_sprite
 		update_configuration_warnings()
 		
-@export var light_source: Node2D:		# Provides light source location
+@export var light_source: Node2D:				# Provides light source location
 	set(a_source):
 		light_source = a_source
 		update_configuration_warnings()
 		
-var _shadow_sprite: Sprite2D			# Internally generated copy of <sprite>
-										# Generates shadow on different z
+var _shadow_sprite: Sprite2D					# Internally generated copy of <sprite>
+												# Generates shadow on different z
 @onready var shadow_shader: Shader = preload("res://shaders/shadow_zoom.gdshader")
 
-var light_source_delta_z: float = 100.0:			# Simulated z-distance between light_source and sprite
+var light_source_delta_z: float = 100.0:		# Simulated z-distance between light_source and sprite
 	set(a_delta):
 		light_source_delta_z = a_delta
 		recalculate_zoom()
@@ -28,21 +28,19 @@ var shadow_delta_z: float = 50.0:				# Simulated z-distance between sprite and s
 		recalculate_zoom()
 		recalculate_blur()
 		
-var zoom: float							# Size of shadow comapred to original
-var blur_amount: float:					# How blurry?
+var zoom: float									# Size of shadow comapred to original
+var blur_amount: float:							# How blurry?
 	set(a_amount):
 		blur_amount = a_amount
 		_shadow_sprite.material.set_shader_parameter("blur_amount", blur_amount)
 
-var alpha: float:						# How transparent?
+var alpha: float:								# How transparent?
 	set(a_alpha):
 		alpha = a_alpha
 		var t_color: Color = _shadow_sprite.material.get_shader_parameter("color")
 		t_color.a = a_alpha
 		_shadow_sprite.material.set_shader_parameter("color", t_color)
 
-#var _source_distance_3d: float				# Pseudo 3D disatnce between light source and obj
-#var _shadow_distance_3d: float				# Pseuado 3D distance between obj and shadow
 
 func initialize():
 	_shadow_sprite = sprite.duplicate()
