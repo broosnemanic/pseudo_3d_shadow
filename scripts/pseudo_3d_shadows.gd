d457d73c16a91b5e2dbf2eca630b573882e7b56c
corpsinhere <broosnemanic@gmail.com>
2025-06-03 02:32:39 -0700

    Cleanup

--- scripts/pseudo_3d_shadows.gd
+++ scripts/pseudo_3d_shadows.gd
@@ -1,45 +1,35 @@
 extends Node2D
 
-var sprite: Sprite2D
+## Example scene to show how to use PseudoShadow3D along with the helper class PseudoNode3D ##
+
 @onready var light_source: PseudoNode3D = %LightSource
 @onready var light_source_2: PseudoNode3D = %LightSource2
 @onready var knight_sprite: Sprite2D = %Knight
 @onready var cat_sprite: Sprite2D = %Cat
-@onready var center: Vector2 = Vector2(500.0, 200.0)
+@onready var _center: Vector2 = Vector2(500.0, 200.0)		# Position for lights to orbit
 
 
 func _ready() -> void:
 	light_source.pseudo_z = 200.0
 	light_source_2.pseudo_z = 500.0
 	add_shadow(knight_sprite, light_source)
-	#add_shadow(cat_sprite, light_source)
+	add_shadow(cat_sprite, light_source)
 	add_shadow(knight_sprite, light_source_2)
-	#add_shadow(cat_sprite, light_source_2)
-
-
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_W:
-			light_source.position += Vector2(0.0, -10.0)
-		if event.keycode == KEY_A:
-			light_source.position += Vector2(-10.0, 0.0)
-		if event.keycode == KEY_S:
-			light_source.position += Vector2(0.0, 10.0)
-		if event.keycode == KEY_D:
-			light_source.position += Vector2(10.0, 0.0)
+	add_shadow(cat_sprite, light_source_2)
 
 
 func _process(delta: float) -> void:
 	var t_secs: float = Time.get_ticks_msec()
-	light_source.position = center + Vector2(250.0 * sin(t_secs / 1000.0), 50.0 * sin(0.5 * PI + t_secs / 500.0))
-	light_source_2.position = center + Vector2(0.0, 100.0) + Vector2(250.0 * sin(t_secs / 1200.0), 0.0)
+	light_source.position = _center + Vector2(250.0 * sin(t_secs / 1000.0), 50.0 * sin(0.5 * PI + t_secs / 500.0))
+	light_source_2.position = _center + Vector2(0.0, 100.0) + Vector2(250.0 * sin(t_secs / 1200.0), 0.0)
 	light_source.get_node("Torch").scale = Vector2(sqrt(light_source.pseudo_z / 2000.0), sqrt(light_source.pseudo_z / 2000.0))
 	light_source_2.get_node("Torch").scale = Vector2(sqrt(light_source_2.pseudo_z / 2000.0), sqrt(light_source_2.pseudo_z / 2000.0))
-	
+
 
 func add_shadow(a_sprite: Sprite2D, a_source: PseudoNode3D):
 	var t_shadow: PseudoShadow3D = PseudoShadow3D.new()
 	t_shadow.sprite = a_sprite
 	t_shadow.light_source = a_source
 	t_shadow.alpha = 0.5
+	t_shadow.name = a_sprite.name + "_shadow_" + str(a_sprite.get_children().size())
 	a_sprite.add_child(t_shadow)
--- scripts/pseudo_shadow_3d.gd
+++ scripts/pseudo_shadow_3d.gd
@@ -1,34 +1,21 @@
-@tool
 extends Sprite2D
 class_name PseudoShadow3D
+
+## Simulates a shadow cast by <sprite> as illumnated by a light source from the z-direction.
+## Calculates zoom level but no distortion
+## Use: create using PseudoShadow3D.new(); set the Sprite2D which will be casting the shadow (<sprite>),
+## Which should usually be the parent of this. Set light_source, which should usually be a child of the Light2D
+## actually casting light. Run initialize()
 
 @export var sprite: Sprite2D:					# The sprite that will be casting a shadow
 	set(a_sprite):
 		sprite = a_sprite
 		initialize()
-		update_configuration_warnings()
-		
+
 @export var light_source: PseudoNode3D:				# Provides light source location
 	set(a_source):
 		light_source = a_source
 		initialize()
-		update_configuration_warnings()
-
-var shadow_shader: Shader = load("res://shaders/shadow_zoom.gdshader")
-
-# Simulated z-distance between obj casting shadow and surface
-# shadow is being cast on (e.g. the wall behind the obj)
-var shadow_delta_z: float = 50.0:
-	set(a_delta):
-		shadow_delta_z = a_delta
-		recalculate_zoom()
-		recalculate_blur()
-		
-var zoom: float									# Size of shadow comapred to original
-var blur_amount: float:							# How blurry?
-	set(a_amount):
-		blur_amount = a_amount
-		material.set_shader_parameter("blur_amount", blur_amount)
 
 @export var alpha: float:								# How transparent?
 	set(a_alpha):
@@ -38,11 +25,28 @@ var blur_amount: float:							# How blurry?
 			t_color.a = a_alpha
 			material.set_shader_parameter("color", t_color)
 
+# Simulated z-distance between obj casting shadow and surface
+# shadow is being cast on (e.g. the wall behind the obj)
+var shadow_delta_z: float = 50.0:
+	set(a_delta):
+		shadow_delta_z = a_delta
+		recalculate_zoom()
+		recalculate_blur()
+		
+var blur_amount: float:							# How blurry?
+	set(a_amount):
+		blur_amount = a_amount
+		material.set_shader_parameter("blur_amount", blur_amount)
+
+var shadow_shader: Shader = load(SHADOW_SHADER_PATH)
+var _zoom: float								# Size of shadow compared to original
+
+const SHADOW_SHADER_PATH: String = "res://shaders/shadow_zoom.gdshader"
 const default_z_delta: int = 10					# Count of z-ordering-levels behind parent
+
 
 func initialize():
 	if sprite != null and light_source != null:
-		alpha = alpha
 		z_index = sprite.z_index - default_z_delta
 		texture = sprite.texture
 		material = ShaderMaterial.new()
@@ -54,30 +58,17 @@ func initialize():
 
 func _process(delta: float) -> void:
 	if material != null:
-		recalculate_zoom()
-		recalculate_blur()
 		material.set_shader_parameter("shadow_offset", shadow_offset())
-		material.set_shader_parameter("zoom", zoom)
+		material.set_shader_parameter("zoom", _zoom)
 
 
 func shadow_offset() -> Vector2:
-	if light_source != null:
-		return (shadow_delta_z / light_source.pseudo_position().z) * (light_source.position - sprite.position) + (zoom - 1) * 0.5 * sprite.texture.get_size()
-	else:
-		return Vector2.ZERO
+	return (shadow_delta_z / light_source.pseudo_position().z) * (light_source.position - sprite.position) + (_zoom - 1) * 0.5 * sprite.texture.get_size()
 
 
 func recalculate_blur():
-	blur_amount = min(5.0, 1.5 * zoom)
-	
-
-func _get_configuration_warnings():
-	if sprite == null:
-		return ["Requires a sprite: Sprite2D"]
-	if light_source == null:
-		return ["Requires a light_source: Node2D"]
+	blur_amount = min(5.0, 1.5 * _zoom)
 
 
 func recalculate_zoom():
-	#zoom = (light_source_delta_z + shadow_delta_z) / light_source_delta_z
-	zoom = (light_source.pseudo_position().z + shadow_delta_z) / light_source.pseudo_position().z
+	_zoom = (light_source.pseudo_position().z + shadow_delta_z) / light_source.pseudo_position().z
